42675d7dbb960d1bdbe964790d70d6a0d5fc7e2f
BrenoMelo <breno_Fabricio23@hotmail.com>
2019-06-17 14:46:43 +0100

    Update create_sef_file.py

--- create_sef_file.py
+++ create_sef_file.py
@@ -1,12 +1,12 @@
 
 import SEF
 
-records = {'ID': 'DWR_Zarate', 'Name': 'Zárate', 'Source': 'NA', 'Lat': '-34.0958', 'Lon': -59.0242,
-           'Link': 'https://data-rescue.copernicus-climate.eu/lso/1086267', 'Vbl': 'msl pressure', 'Stat': 0,
-           'Units': 'hPa', 'Meta': 'Alias=Zarate|PTC=T|PGC=?|Data policy=Open', 'Year': [2000, 2000], 'Month': [1, 2], 'Day': [2, 3],
-           'Hour': [10, 11], 'Minute': [20, 31], 'Value': [0, 1], 'Period': [0, 1], 'Meta2': ['Orig.=754.5mm|Orig.time=', 'Orig.=754.5mm|Orig.time='],
-           'orig_time': ['14:00', '14:00']}
+records = {'ID': 'DWR_Zarate', 'Name': 'Zárate', 'Source': 'C3S_SouthAmerica', 'Lat': '-34.0958', 'Lon': -59.0242,
+           'Link': 'https://data-rescue.copernicus-climate.eu/lso/1086267', 'Vbl': 'mslp', 'Stat': 'point',
+           'Units': 'Pa', 'Meta': 'Data policy=GNU GPL v3.0', 'Year': [1902, 1902], 'Month': [9, 9], 'Day': [2, 3],
+           'Hour': [11, 11], 'Minute': [17, 17], 'Value': [102245, 101952], 'Period': [0, 0], 'Meta2': ['Orig=766.9mm|Orig.time=', 'Orig.=764.7mm|Orig.time='],
+           'orig_time': ['7am', '7am']}
 
 obs = SEF.create(records)
 
-SEF.write_file(obs, '/PATH/SEF-master/examples/output.tsv')
+SEF.write_file(obs, '/PATH/examples/output.tsv')
