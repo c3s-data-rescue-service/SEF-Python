3a871781a819e9d4aee4d4fb0dd74f1e4c3cb57b
BrenoMelo <breno_Fabricio23@hotmail.com>
2019-07-16 13:51:26 +0100

    Update create_sef_file.py

--- create_sef_file.py
+++ create_sef_file.py
@@ -6,7 +6,7 @@ records = {
            'Link': 'https://data-rescue.copernicus-climate.eu/lso/1086267', 'Vbl': 'mslp', 'Stat': 'point',
            'Units': 'Pa', 'Meta': 'Data policy=GNU GPL v3.0', 'Year': [1902, 1902], 'Month': [9, 9], 'Day': [2, 3],
            'Hour': [11, 11], 'Minute': [17, 17], 'Value': [102245, 101952], 'Period': [0, 0], 
-           'Meta2': ['Orig=766.9mm|Orig.time=', 'Orig.=764.7mm|Orig.time='], 'orig_time': ['7am', '7am']
+           'Meta2': ['Orig=766.9mm|Orig.time=', 'Orig.=764.7mm|Orig.time='], 'offset': -4
 }
 
 obs = SEF.create(records)
