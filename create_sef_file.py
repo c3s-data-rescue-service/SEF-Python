a4546e5f61b852b297267911d8a2f6144c5bd149
BrenoMelo <breno_Fabricio23@hotmail.com>
2020-03-04 10:28:01 +0000

    Update create_sef_file.py

--- create_sef_file.py
+++ create_sef_file.py
@@ -1,14 +1,26 @@
 
+import os.path
+
 import SEF
 
-records = {
-           'ID': 'DWR_Zarate', 'Name': 'Zárate', 'Source': 'C3S_SouthAmerica', 'Lat': '-34.0958', 'Lon': -59.0242,
-           'Link': 'https://data-rescue.copernicus-climate.eu/lso/1086267', 'Vbl': 'mslp', 'Stat': 'point',
-           'Units': 'Pa', 'Meta': 'Data policy=GNU GPL v3.0', 'Year': [1902, 1902], 'Month': [9, 9], 'Day': [2, 3],
-           'Hour': [11, 11], 'Minute': [17, 17], 'Value': [102245, 101952], 'Period': [0, 0], 
-           'Meta2': ['Orig=766.9mm|Orig.time=', 'Orig.=764.7mm|Orig.time='], 'offset': -4
-}
+THIS_PATH = os.path.abspath(os.path.dirname(__file__))
+OUTPUT_FOLDER = os.path.join(THIS_PATH, 'output')
+OUTPUT_PATH = os.path.join(OUTPUT_FOLDER, 'output_file.tsv')
 
-obs = SEF.create(records)
 
-SEF.write_file(obs, '/PATH/examples/output.tsv')
+def create_output_file():
+    records = {
+        'ID': 'DWR_Zarate', 'Name': 'Zárate', 'Source': 'C3S_SouthAmerica', 'Lat': '-34.0958', 'Lon': -59.0242,
+        'Link': 'https://data-rescue.copernicus-climate.eu/lso/1086267', 'Vbl': 'mslp', 'Stat': 'point',
+        'Units': 'Pa', 'Meta': 'Data policy=GNU GPL v3.0', 'Year': [1902, 1902], 'Month': [9, 9], 'Day': [2, 3],
+        'Hour': [11, 11], 'Minute': [17, 17], 'Value': [102245, 101952], 'Period': [0, 0],
+        'Meta2': ['Orig=766.9mm|Orig.time=', 'Orig.=764.7mm|Orig.time='], 'offset': -4
+    }
+
+    obs = SEF.create(records)
+
+    SEF.write_file(obs, OUTPUT_PATH)
+
+
+if __name__ == "__main__":
+    create_output_file()
